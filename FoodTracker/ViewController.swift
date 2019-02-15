0968c5e77021966b9d8236db7dda5cd388c542c4
Paweł Krzykała <pawel.krzykala@cloudity.pl>
2019-02-15 20:19:35 +0100

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Paweł Krzykała on 15/02/2019.
+//  Copyright © 2019 Paweł Krzykała. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
