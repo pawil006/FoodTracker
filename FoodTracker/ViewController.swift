7ebae47ee7d11f57558cd9860fce2ef9435d9a1d
Paweł Krzykała <pawel.krzykala@cloudity.pl>
2019-02-17 19:48:37 +0100

    Added Image selector and allowed to use photo library.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +20,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         nameTextField.delegate = self
     }
 
-    //MARK: Actions
+    
     @IBAction func setDafaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
@@ -28,6 +29,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
         //this method throws an event to hide keyboard
         textField.resignFirstResponder()
         return true
+    }
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //dismiss the picker
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //check if info contain original image, if not creates an error
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image but was provided the following \(info)")
+        }
+        photoImageView.image = selectedImage
+        // dismiss a picker
+        dismiss(animated: true, completion: nil)
+    }
+    //MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        //image picker controller is stuff to select photo from gallery
+        let imagePickerController = UIImagePickerController()
+        // and set source type only to gallery
+        imagePickerController.sourceType = .photoLibrary
+        // set IPC to delegate to VC
+        imagePickerController.delegate = self
+        // opens library selector
+        present(imagePickerController, animated: true, completion: nil)
+        
     }
     func textFieldDidEndEditing(_ textField: UITextField) {
         mealNameLabel.text = textField.text
