34d535a9ceaf13ec2819e9b0f7be3a04f92af375
Paweł Krzykała <pawel.krzykala@cloudity.pl>
2019-02-18 21:09:59 +0100

    Added unit tests to meal and changed init condition to series of guard

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -16,7 +16,13 @@ class Meal {
     var rating: Int
     
     init?(name: String, photo: UIImage?, rating: Int) {
-        if name.isEmpty || rating < 0 {
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
             return nil
         }
         self.name = name
--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -10,25 +10,24 @@ import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    //MARK: Meal Test Class
+    // test if meal init returns correct value when correct arguments
+    func testMealInitialisationSucceeds() {
+        // zero rating
+        let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingMeal)
+        // highest positive rating
+        let positiveRatingMeal = Meal.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingMeal)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    //test negative case
+    func testMealInitializationFails() {
+        let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingMeal)
+        let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingMeal)
+        let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringMeal)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
